c0b34072ed297ebd3b45acdf7bda5976e6e8bf38
miguel-lima-tech <miguelvaqueirogamet@gmail.com>
2026-05-20 13:57:38 -0300

    feat: classe abstrata Transacao

---
+++ src/model/Transacao.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.time.LocalDate;
+
+public abstract class Transacao {
+
+    private String descricao;
+    private double valor;
+    private LocalDate data;
+
+    public Transacao(String descricao, double valor, LocalDate data) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public abstract double getValorParaSaldo();
+}
