ee8b3008b26815ace2bb43bf1b586499e59c73e3
miguel-lima-tech <miguelvaqueirogamet@gmail.com>
2026-05-20 14:40:34 -0300

    feat: enum categoria

---
+++ src/model/Categoria.java
@@ -0,0 +1,12 @@
+package model;
+
+public enum Categoria {
+    ALIMENTACAO,
+    TRANSPORTE,
+    MORADIA,
+    LAZER,
+    SAUDE,
+    EDUCACAO,
+    SALARIO,
+    OUTROS
+}
--- src/model/Transacao.java
+++ src/model/Transacao.java
@@ -7,11 +7,13 @@ public abstract class Transacao {
     private String descricao;
     private double valor;
     private LocalDate data;
+    private Categoria categoria;
 
-    public Transacao(String descricao, double valor, LocalDate data) {
+    public Transacao(String descricao, double valor, LocalDate data, Categoria categoria) {
         this.descricao = descricao;
         this.valor = valor;
         this.data = data;
+        this.categoria = categoria;
     }
 
     public String getDescricao() {
@@ -36,6 +38,14 @@ public abstract class Transacao {
 
     public void setData(LocalDate data) {
         this.data = data;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
     }
 
     public abstract double getValorParaSaldo();
