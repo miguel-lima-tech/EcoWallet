b8ae6224ea77dae7620923312215e9cb3d64b469
miguel-lima-tech <miguelvaqueirogamet@gmail.com>
2026-05-21 09:52:25 -0300

    fix: atualizar subclasses com categoria

--- src/model/Despesa.java
+++ src/model/Despesa.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Despesa extends Transacao {
 
-    public Despesa(String descricao, double valor, LocalDate data) {
-        super(descricao, valor, data);
+    public Despesa(String descricao, double valor, LocalDate data, Categoria categoria) {
+        super(descricao, valor, data, categoria);
     }
 
     @Override
--- src/model/Receita.java
+++ src/model/Receita.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Receita extends Transacao {
 
-    public Receita(String descricao, double valor, LocalDate data) {
-        super(descricao, valor, data);
+    public Receita(String descricao, double valor, LocalDate data, Categoria categoria) {
+        super(descricao, valor, data, categoria);
     }
 
     @Override
