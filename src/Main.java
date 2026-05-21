383966edf689199e34daf2d6cd1a022587ac8327
miguel-lima-tech <miguelvaqueirogamet@gmail.com>
2026-05-21 10:09:03 -0300

    test: validacao das transacoes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.time.LocalDate;
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("EcoWallet iniciado!");
+
+        GerenciadorFinancas gerenciador = new GerenciadorFinancas();
+
+        Transacao salario = TransacaoFactory.criarTransacao(
+                "Receita",
+                "Salário",
+                1500.00,
+                LocalDate.now(),
+                Categoria.SALARIO
+        );
+
+        Transacao mercado = TransacaoFactory.criarTransacao(
+                "Despesa",
+                "Compra no mercado",
+                250.00,
+                LocalDate.now(),
+                Categoria.ALIMENTACAO
+        );
+
+        gerenciador.adicionarTransacao(salario);
+        gerenciador.adicionarTransacao(mercado);
+
+        System.out.println("Transações cadastradas:");
+        for (Transacao t : gerenciador.getTransacoes()) {
+            System.out.println(t.getDescricao() + " - R$ " + t.getValorParaSaldo());
+        }
+
+        System.out.println("Saldo total: R$ " + gerenciador.calcularSaldo());
     }
 }
